f9f23c5af664e4ab63fe5ba1a1094e9740569c47
Seung-Joon <tmdwns02556@gmail.com>
2019-04-01 13:12:28 +0900

    Update dust_measure_system_firmware.ino

--- dust_measure_system_firmware/dust_measure_system_firmware.ino
+++ dust_measure_system_firmware/dust_measure_system_firmware.ino
@@ -1,3 +1,43 @@
+//     Version 1.0                     
+//     Last updated at 2019.04.01      
+//     Made by Seung-Jun Park          
+//                                     
+//     - Using Sensor  list -              
+//       1. PMS7003                    
+//       2. DHT22                      
+//       3. Voltage sensor 1 (Using for system batter voltage measure), (consist of 7500R, 30000R)       
+//       4. voltage sensor 2 (Using for solar panel voltage measure), (consist of 10000K,        
+//                                   
+//     - Pin list -                
+//        D13: DHT22
+//        D9: SoftWareSerialPin(Tx)     
+//        D8: SoftWareSerialPin(Rx)  
+//        D7: LCD_RS_pin               
+//        D6: LCD_EN_pin    
+//        D5: LCD_data4_pin
+//        D4: LCD_data5_pin
+//        D3: LCD_data6_pin
+//        D2: LCD_data7_pin
+//        A7: System_voltage_measure_pin
+//        A6: Solar_panel_voltage_measure_pin
+
+/*      - Bit composition - (configurated by hexa code)
+             {STARTING_BIT,
+              SYSTEM_ID,
+              PM1.0,
+              PM2.5,
+              PM10,
+              Float_FLAG,
+              SYSTEM_VOLTAGE,
+              VOLTAGE_RESOLUTION,
+              Float_FLAG,
+              SOLAR_VOLTAGE,
+              SOLAR_RESOLUTION,
+              TEMPURATURE,
+              HUMIDITY,
+              ENDBIT
+              }*/
+
 #include <SoftwareSerial.h>
 #include <LiquidCrystal.h>
 #include "DHT.h"
@@ -97,7 +137,7 @@ void getDustData(struct dust_data &d) {
         if (incomingByte == 0x42 && detectOff == 0) {
           frameBuf[detectOff] = incomingByte;
           d.frameHeader[0] = incomingByte;
-          calcChecksum = incomingByte; // Checksum init!
+          calcChecksum = incomingByte; // Checksum init
           detectOff++;
         }
 
@@ -180,8 +220,6 @@ void lcd_drawMain(struct system_data sys, struct dust_data D_data) {
 void DATA_TRANSMITTION(struct system_data sys, struct dust_data D_data) {
   char tempBuffer[64];
   sprintf(tempBuffer, "%x %x %x %x %x %x %x %x %x %x %x %x %x %x", STARTING_BIT, SYSTEM_ID, D_data.pm1, D_data.pm2_5, D_data.pm10, FLAG_FLOT, int(sys.v_battery * 100), 100, FLAG_FLOT, int(sys.v_solar * 100), 100, int(sys.tempurature), int(sys.humidity), int(END_BIT));
-  // Bit composition (configurated by hexa code)
-  // STARTING_BIT|SYSTEM_ID|PM1.0|PM2.5|PM10|Float_FLAG|SYSTEM_VOLTAGE|VOLTAGE_RESOLUTION|Float_FLAG|SOLAR_VOLTAGE|SOLAR_RESOLUTION|TEMPURATURE|HUMIDITY|ENDBIT
   Serial.print(tempBuffer);
   return;
 }
@@ -193,7 +231,7 @@ void setup() {
   lcd.createChar(0, customChar);
 }
 
-int k = 1; //convert write 
+int k = 1; //convert to write 
 
 void loop() {
   getDustData(dust_data);
